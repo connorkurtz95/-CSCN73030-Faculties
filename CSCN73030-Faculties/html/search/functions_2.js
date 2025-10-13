1d05f971728b2420326c3a5110cf5b4a567c6661
RhysHunt <rhunt2963@conestogac.on.ca>
2025-10-13 16:24:18 -0400

    Doxygen added and code simplified
    Doxygen documentation added into the part.h file and part.h code simplified to no longer use an Enum for the part state

---
+++ CSCN73030-Faculties/html/search/functions_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['setaquisitiondate_0',['SetAquisitionDate',['../class_part.html#a64c63cedee0cb0acec9ac1524dfc10f0',1,'Part']]],
+  ['setpartdesc_1',['SetPartDesc',['../class_part.html#a20922c756079838c4a7d9a0199763818',1,'Part']]],
+  ['setpartid_2',['SetPartID',['../class_part.html#a3900d9e7a3bb6911486f03a805c73e0e',1,'Part']]],
+  ['setparttype_3',['SetPartType',['../class_part.html#ad658dfaab3b68cb0efe3226dea6ed1d5',1,'Part']]]
+];
